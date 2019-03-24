ba71c9a7bb8d9093c59985eb931402df7664c8da
SteveMustanski <steve.mustanski@gmail.com>
2019-03-24 14:15:32 -0400

    used radium to style the application

--- src/app.js
+++ src/app.js
@@ -1,29 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { Style } from 'radium';
 
-import Carousel from './carousel.js'
-import Frame from './frame.js'
-import Nav from './nav.js'
-import Slide from './slide.js'
+import Carousel from './carousel.js';
+import Frame from './frame.js';
+import Nav from './nav.js';
+import Slide from './slide.js';
+import configStyles from './config-styles';
+import styles from './app-styles';
 
 export default class DriftApp extends React.Component {
   constructor(props) {
-    super(props)
-    this.handleClickPrevious = this.handleClickPrevious.bind(this)
-    this.handleClickNext = this.handleClickNext.bind(this)
+    super(props);
+    this.handleClickPrevious = this.handleClickPrevious.bind(this);
+    this.handleClickNext = this.handleClickNext.bind(this);
     this.state = {
       showIndex: 0,
-      numSlides: 5
-    }
+      numSlides: 5,
+    };
   }
   handleClickPrevious() {
     this.setState({
-      showIndex: Math.max(this.state.showIndex - 1, 0)
-    })
+      showIndex: Math.max(this.state.showIndex - 1, 0),
+    });
   }
   handleClickNext() {
     this.setState({
-      showIndex: Math.min(this.state.showIndex + 1, this.state.numSlides - 1)
-    })
+      showIndex: Math.min(this.state.showIndex + 1, this.state.numSlides - 1),
+    });
   }
   renderNav() {
     return (
@@ -33,44 +36,45 @@ export default class DriftApp extends React.Component {
         onNext={this.handleClickNext}
         hasNext={this.state.showIndex < this.state.numSlides - 1}
       />
-    )
+    );
   }
   render() {
     return (
       <Frame>
+        <Style rules={styles} />
         <Carousel
           showIndex={this.state.showIndex}
           nav={this.renderNav()}
-          width={640}
+          width={configStyles.imageWidth}
         >
-          <Slide image={require('./images/1.jpg')} title="Imperial Mockery">
+          <Slide image={require('./images/1.jpg')} title='Imperial Mockery'>
             In a show of defiance, rebels have again made mockery of the majesty
             that is service to the Empire. These objects were immediately
             removed from the reflecting pool in Coruscant's Central Square when
             found this morning.
           </Slide>
-          <Slide image={require('./images/2.jpg')} title="Trooper Initiation">
+          <Slide image={require('./images/2.jpg')} title='Trooper Initiation'>
             In a tradition that predates the Empire, Troopers seen here are made
             to suffer the agony of the limbo line. Such initiation techniques
             are currently under Imperial review.
           </Slide>
           <Slide
             image={require('./images/3.jpg')}
-            title="Master and Apprentice"
+            title='Master and Apprentice'
           >
             Internships work! When determining a career path, it is wise to
             first find someone currently in that line of work. They will likely
             be able to show you what it's really like before you commit your
             future.
           </Slide>
-          <Slide image={require('./images/4.jpg')} title="Battle of Coruscant">
+          <Slide image={require('./images/4.jpg')} title='Battle of Coruscant'>
             Modelers ran out of the pre-Empire model Star Destroyers in this
             reenactment of the Battle of Coruscant. Various plans for future
             machines of war were used as filler.
           </Slide>
           <Slide
             image={require('./images/5.jpg')}
-            title="The Wiles of the Forest"
+            title='The Wiles of the Forest'
           >
             After a number of nervous breakdowns, Empire personnel officers are
             investigating the possibility rotating troopers stationed on the
@@ -78,6 +82,6 @@ export default class DriftApp extends React.Component {
           </Slide>
         </Carousel>
       </Frame>
-    )
+    );
   }
 }
--- src/nav.js
+++ src/nav.js
@@ -1,20 +1,37 @@
-import PropTypes from 'prop-types'
-import React from 'react'
+import PropTypes from 'prop-types';
+import React from 'react';
+import styles from './nav-styles';
+import Radium from 'radium';
+
+function getPrevStyles(props) {
+  return props.hasPrevious ? styles.prev : styles.prevHidden;
+}
+function getNextStyles(props) {
+  return props.hasNext ? styles.next : styles.prevHidden;
+}
 
 function Nav(props) {
   return (
-    <div>
-      <button onClick={props.onPrevious}>&#10094;</button>
-      <button onClick={props.onNext}>&#10095;</button>
+    <div style={styles.root}>
+      <button
+        key='prev'
+        style={getPrevStyles(props)}
+        onClick={props.onPrevious}
+      >
+        &#10094;
+      </button>
+      <button key='next' style={getNextStyles(props)} onClick={props.onNext}>
+        &#10095;
+      </button>
     </div>
-  )
+  );
 }
 
 Nav.propTypes = {
   onPrevious: PropTypes.func.isRequired,
   onNext: PropTypes.func.isRequired,
   hasPrevious: PropTypes.bool,
-  hasNext: PropTypes.bool
-}
+  hasNext: PropTypes.bool,
+};
 
-export default Nav
+export default Radium(Nav);
